6f1df08567d2dc75df9b18091d6defc56aa50107
nakayama_takahito <takahito.nakayama@qnote.co.jp>
2022-08-15 11:51:00 +0900

    アラーム項目のスケルトンを作成

--- lib/alarm_route.dart
+++ lib/alarm_route.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AlarmPage extends HookConsumerWidget {
@@ -13,7 +14,111 @@ class AlarmPage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text('アラーム'),
       ),
-      body: Container(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 160,
+            width: double.infinity,
+            child: Card(
+              color: Colors.white, // Card自体の色
+              margin: const EdgeInsets.all(30),
+              elevation: 8, // 影の離れ具合
+              shadowColor: Colors.black ,// 影の色
+              shape: RoundedRectangleBorder( // 枠線を変更できる
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: InkWell(
+                onTap: () {},
+                child: ListTile(
+                  dense: true,
+                  leading: Checkbox(
+                      value: true,
+                      onChanged: (newValue) {
+
+                      },
+                    ),
+                    title: const Text(
+                      '06:30',
+                      style: TextStyle(
+                          fontSize: 17
+                      ),
+                    ),
+                    subtitle: const Text(
+                      'スヌーズ：あり\n'
+                          '繰り返し：毎日\n'
+                          'サウンド：　　\n'
+                          'バイブレーション：あり',
+                      style: TextStyle(
+                      fontSize: 10
+                    ),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {  },
+                    icon: const Icon(
+                      Icons.more_vert,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            // height: 160,
+            width: double.infinity,
+            child: Card(
+              color: Colors.white, // Card自体の色
+              margin: const EdgeInsets.all(30),
+              elevation: 8, // 影の離れ具合
+              shadowColor: Colors.black ,// 影の色
+              shape: RoundedRectangleBorder( // 枠線を変更できる
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: InkWell(
+                onTap: () {},
+                child: ExpansionTile(
+                  tilePadding: EdgeInsets.zero,
+                  childrenPadding: EdgeInsets.all(8),
+                  leading: Checkbox(
+                    value: true,
+                    onChanged: (newValue) {
+
+                    },
+                    materialTapTargetSize: MaterialTapTargetSize.padded,
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {  },
+                    icon: const Icon(
+                      Icons.more_vert,
+                    ),
+                  ),
+                  title: const Text(
+                    '06:30',
+                    style: TextStyle(
+                        fontSize: 17
+                    ),
+                  ),
+                  subtitle: const Text(
+                    '繰り返し：月火水木金土日',
+                    style: TextStyle(
+                        fontSize: 10
+                    ),
+                  ),
+                  onExpansionChanged: (bool changed) {
+
+                  },
+                  expandedCrossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text(
+                      'スヌーズ：あり\n'
+                      'サウンド：　　\n'
+                      'バイブレーション：あり',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
